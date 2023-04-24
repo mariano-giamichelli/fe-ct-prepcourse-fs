6c3d7b9ab176236774b9a030fae8f9146859a594
mariano-giamichelli <marianogiamichelli@gmail.com>
2023-04-24 17:51:44 -0300

    actualizacon de ejercicio hechos

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,47 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array[0]
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   var n=array.length
+   return array[n-1]
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   return array.length
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   for(var n=0; n<array.length; n++)
+   {
+      array[n]=array[n]+1
+   }
+   return array
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push(elemento)
+   return array
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento)
+   return array
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,30 +52,65 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+   return palabras.join(' ')
 }
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar true si está, o false si no está.
    // Tu código:
+   // caso 1:
+   /*for(var i=0; i<array.length; i++)
+   {
+      if(array[i] === elemento)
+      {
+         return true
+      }
+   }
+   return false
+   */
+  //caso 2
+   return array.includes(elemento)
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   var resultado=0
+   for(var i=0; i<arrayOfNums.length;i++)
+   {
+      resultado = resultado + arrayOfNums[i]
+   }
+   return resultado
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   var promedio=0
+   for(var i=0; i<resultadosTest.length; i++)
+   {
+      promedio = promedio + resultadosTest[i]
+   }
+   promedio = promedio / resultadosTest.length
+   return promedio
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   var max=0;
+   for (var i=0; i<arrayOfNums.length; i++)
+   {
+      if(arrayOfNums[i]>max)
+      {
+         max=arrayOfNums[i]
+      }
+   }
+   return max
 }
 
 function multiplicarArgumentos() {
@@ -70,11 +118,33 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   var mult=1;
+   if(arguments.length === 0)
+   {
+      return 0
+   } else if(arguments.length === 1)
+          {
+            return arguments[0]
+          } 
+   for (var i=0; i<arguments.length; i++)
+   {
+      mult=mult*arguments[i]
+   }
+   return mult
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   var j=0;
+   for (var i=0; i<array.length; i++)
+   {
+      if(array[i]>18)
+      {
+         j++
+      }
+   }   
+   return j
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -82,18 +152,36 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+   if(numeroDeDia === 1 || numeroDeDia === 7)
+   {
+      return "Es fin de semana"
+   } else return "Es dia laboral"
 }
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar true si el entero inicia con 9 y false en otro caso.
    // Tu código:
+   var converNumEnString = String(num) // Para convertir un variable en caracteres (string)
+   var nueve = converNumEnString.split('')
+   if (nueve[0] === '9')
+   {
+      return true
+   } else return false
 }
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
+   for (var i=1; i<array.length; i++)
+   {
+      if(array[i] === array[i-1])
+      {
+         return true
+      }
+   }
+   return false
 }
 
 function mesesDelAño(array) {
@@ -101,18 +189,50 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   var arraynuevo = [];
+   var j=0;
+   for (var i=0; i<array.length; i++)
+   {
+      if( array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre")
+      {
+         arraynuevo[j]=array[i]
+         j++
+         if(j === 3)
+         {
+            return arraynuevo
+         }
+      }
+   }
+   return "No se encontraron los meses pedidos"
 }
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   var tabla = []
+   for (var i=0; i<11; i++)
+   {
+      tabla[i] = i * 6
+   }
+   return tabla
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+   var j=0;
+   var nuevoarray = []
+   for (var i=0; i<array.length; i++)
+   {
+      if(array[i] > 100)
+      {
+         nuevoarray[j]=array[i]
+         j++
+      }
+   }
+   return nuevoarray
 }
 
 /* ----------------------------------------------------------------------------------
@@ -126,6 +246,27 @@ function breakStatement(num) {
    // la ejecución y retornar el string: "Se interrumpió la ejecución".
    // [PISTA]: utiliza el statement 'break'.
    // Tu código:
+   var nuevoarray = []
+   var j=0;
+   for (var i=0; i<10; i++)
+   {  
+      if(i==0)
+      {
+      nuevoarray[i] = num + 2  
+      } else if (i>0)
+         {
+            nuevoarray[i]=nuevoarray[i-1]+2
+         }
+      if (nuevoarray[i] === i)
+      {
+         break
+      }
+      j++
+   }  
+   if(j<10)
+   {
+      return "Se interrumpió la ejecución"
+   } else return nuevoarray 
 }
 
 function continueStatement(num) {
@@ -135,6 +276,27 @@ function continueStatement(num) {
    // se continua con la siguiente iteración.
    // [PISTA]: utiliza el statement 'continue'.
    // Tu código:
+   var nuevoarray = []
+   for (var i=0; i<10; i++)
+   {  
+      if(i==0)
+      {
+      nuevoarray[i] = num + 2  
+      } else if (i>0)
+         {
+            if (i<5)
+            {
+               nuevoarray[i]=nuevoarray[i-1]+2
+            } else if(i===5)
+            {
+               continue
+            } else if (i>5)
+            {
+               nuevoarray[i-1]=nuevoarray[i-2]+2
+            }
+         }
+   }  
+   return nuevoarray
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,6 +7,18 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+  // Crear un nuevo objeto (primera solicitud) con la propiedad "nombre" (es la propiedad (la propiedad es la 
+  // llamada clave) que se encuentra contenido adentro de objeto) y el valor definido como 
+  //el argumento "nombre" (es el argumento que va a ir a la propiedad).
+  // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+  // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
+  // Devuelve el objeto
+  // Tu código:
+  //el objeto se declara como variable y el contenido va en corchete
+  //dos datos importante estructura clave: valor y al finalizar la coma
+  //funcion no tiene nombre en este caso
+   var animal = {nombre: nombre, edad: edad, meow: function () {return "Meow!"}}
+   return animal
 }
 
 function nuevoUsuario(nombre, email, password) {
@@ -14,6 +26,8 @@ function nuevoUsuario(nombre, email, password) {
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
    // Tu código:
+   var registro = {nombre: nombre, email: email, password: password}
+   return registro
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +36,8 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   objeto[propiedad] = null
+   return objeto
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,36 +45,62 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+   objeto[metodo]()
+   //Explicacion de este ejercicio: Recuerda un método es una función, solo que se le llama método.
+   //Una función se invoca declarándola así funcion()
+   //El ejercicio te pide que invoques un método al objeto (ambos por parámetro)
+   //Y la única manera de invocar la propiedad recibida por parametro es bracket notation
+   //Así las cosas objeto[EL NOMBRE RECIBIDO POR PARÁMETRO]() 
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
+   let mult=0
+   mult = objetoMisterioso.numeroMisterioso * 5
+   return mult
 }
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+   delete objeto[propiedad]
+   return objeto
 }
 
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objetoUsuario.email) {
+      return true
+   } else {
+      return false
+   }  
 }
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objeto.hasOwnProperty(propiedad))
+   {
+      return true
+   }  else return false
+  //El método hasOwnProperty() nos permitirá especificar un nombre, y verificar si este
+   // existe como una propiedad dentro de un objeto. En cada caso devolverá true o false. 
 }
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objetoUsuario.password === password)
+   {
+      return true
+   }  else return false
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
@@ -66,6 +108,8 @@ function actualizarPassword(objetoUsuario, nuevaPassword) {
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.password = nuevaPassword
+   return objetoUsuario
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
@@ -73,6 +117,8 @@ function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.amigos.push(nuevoAmigo)
+   return objetoUsuario
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -80,7 +126,12 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Cada usuario tiene una propiedad llamada "esPremium".
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
-   // Tu código:
+   // Tu código:´
+   for(var i=0; i<objetoMuchosUsuarios.length; i++)
+   {
+   objetoMuchosUsuarios[i].esPremium=true
+   }
+   return objetoMuchosUsuarios
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -89,6 +140,12 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
+   var suma=0;
+   for(var i=0; i<objetoUsuario.posts.length; i++)
+   {
+      suma = suma + objetoUsuario.posts[i].likes
+   }
+   return suma
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -102,6 +159,10 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+   objetoProducto.calcularPrecioDescuento = function(){
+   return this.precio - this.precio * this.porcentajeDeDescuento
+   }
+   return objetoProducto
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
